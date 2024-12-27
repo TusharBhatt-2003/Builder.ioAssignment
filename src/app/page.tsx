7a4f6668ef8182cf8c93846d065c4e828b2f4305
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2024-12-27 16:51:31 +0530

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 import Category from "@/components/Category/Category";
-import Featured from "@/components/Featured/Featured";
-import Hero from "@/components/Hero/Hero";
+
 import Search from "@/components/Search/Search";
 
 export default function Home() {
   return (
     <>
-      <Hero />
+      {/* <Hero /> */}
       <div className="flex justify-between items-center max-w-5xl mx-auto mt-6">
         <Category />
         <Search />
