32abb988e1fef1a6f006ab83fa375d798ac538b4
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2024-12-31 18:00:31 +0530

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,4 @@
 "use client";
-import Category from "@/components/Category/Category";
-
-import Search from "@/components/Search/Search";
-
 export default function Home() {
-  return (
-    <>
-      {/* <Hero /> */}
-      <div className="flex justify-between items-center max-w-5xl mx-auto mt-6">
-        <Category />
-        <Search />
-      </div>
-    </>
-  );
+  return <></>;
 }
