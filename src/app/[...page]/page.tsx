61596db99cc42356c2f825082ee008fe45150b6e
Tushar Bhatt <112729468+TusharBhatt-2003@users.noreply.github.com>
2024-12-31 17:41:44 +0530

    Update

--- src/app/[...page]/page.tsx
+++ src/app/[...page]/page.tsx
@@ -1,21 +1,27 @@
 import { builder } from "@builder.io/sdk";
 import { RenderBuilderContent } from "../../components/builder";
+
 // Builder Public API Key set in .env file
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
 interface PageProps {
-  params: Promise<{ page: string[] }>;
+  params: Promise<{
+    page: string[];
+  }>;
 }
 
 export default async function Page(props: PageProps) {
+  // Resolve the promise for params if needed
   const resolvedParams = await props.params;
+
   const builderModelName = "page";
+
   const content = await builder
     // Get the page content from Builder with the specified options
     .get(builderModelName, {
       userAttributes: {
         // Use the page path specified in the URL to fetch the content
-        urlPath: "/" + (props?.params?.page?.join("/") || ""),
+        urlPath: "/" + (resolvedParams?.page?.join("/") || ""),
       },
     })
     // Convert the result to a promise
